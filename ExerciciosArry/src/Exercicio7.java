1777e5b46e7289101d93a7877a3b93ff6356e0b4
Samuel Rodrigues <164402437+SamuelProfissional@users.noreply.github.com>
2024-09-17 21:02:19 -0300

    Update Exercicio7.java

--- ExerciciosArry/src/Exercicio7.java
+++ ExerciciosArry/src/Exercicio7.java
@@ -5,7 +5,19 @@ public class Exercicio7 {
 gere sua equivalente [2x3] transposta.
 Conforme exemplo, utilize o algoritmo do exercício 6
 para imprimir a matriz
-original e a transposta.*/
+original e a transposta.
+   matriz [3x2]  
+      0 1
+    0 a f
+    1 b e
+    2 c d
+    
+    matriz [2x3] 
+      0 1 2 
+    0 a b c
+    1 f e d
+    
+*/
 
 public static void main(String[] args) {
     
