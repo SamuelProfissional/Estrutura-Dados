a1158cafdfa021c3969045a62d907bd780f1aec8
Samuel Rodrigues <164402437+SamuelProfissional@users.noreply.github.com>
2024-09-23 15:33:24 -0300

    Update Exercicio7.java

--- ExerciciosArry/src/Exercicio7.java
+++ ExerciciosArry/src/Exercicio7.java
@@ -1,7 +1,9 @@
 package ExerciciosArry.src;
 
 public class Exercicio7 {
-/*Crie um método que dado uma matriz [3x2] 
+  
+ /*OK
+Crie um método que dado uma matriz [3x2] 
 gere sua equivalente [2x3] transposta.
 Conforme exemplo, utilize o algoritmo do exercício 6
 para imprimir a matriz
@@ -18,32 +20,32 @@ original e a transposta.
     0 a b c
     1 f e d
     
-*/
+     */
+    public static void main(String[] args) {
+        //    matriz [3x2] 
+        String[][] a = new String[][]{{"a", "f"}, {"b", "e"}, {"c", "d"}};
+        mudaMatriz(a);
 
-public static void main(String[] args) {
-    //    matriz [3x2] 
-    String [][] a = new String[][]{{"a","f"},{"b","e"},{"c","d"}};
-    
-}
-public static int mudaMatriz(int matriz){
-    
-    String[][] convertido = new String[2][3];
-    
-    for (int l = 0; l < 3; l++) {
-        
-        for (int c = 0; c < 2; c++) {
-             convertido[0][l] = matriz[l][0];
-             convertido[1][l] = matriz[l][1];
-            
-        }
-        
     }
-    
-    
-    /*if(){
 
-    }else */
-    return 0;
-}
-}
+    public static String[][] mudaMatriz(String[][] matriz) {
+
+        String[][] convertido = new String[2][3];
+
+        for (int l = 0; l < 2; l++) {
+
+            for (int c = 0; c < 3; c++) {
+                convertido[l][c] = matriz[c][l];
+
+            }
+
+        }
+        for (int c = 0; c < 3; c++) {
+
+            System.out.println(convertido[0][c] + " " + convertido[1][c] + " ");
+
+        }
+
+        return convertido;
+    }
 }
